3ae79dc5b449fef1a72b31f186e6a2d1cc13ab09
alflix <jieyuanz24@gmail.com>
2019-11-27 14:55:01 +0800

    update swiftClassFromString

--- Source/NSObject/NSObject+Convenient.swift
+++ Source/NSObject/NSObject+Convenient.swift
@@ -5,17 +5,15 @@
 //  Created by John on 2019/5/22.
 //  Copyright © 2018 GGUI. All rights reserved.
 //
+
 import Foundation
 
+// https://medium.com/@maximbilan/ios-objective-c-project-nsclassfromstring-method-for-swift-classes-dbadb721723
 public extension NSObject {
-    // create a static method to get a swift class for a string name
     static func swiftClassFromString(_ className: String) -> AnyClass! {
-        // get the project name
         if let appName: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String {
-            // generate the full name of your class (take a look into your "YourProject-swift.h" file)
-            let classStringName = "_TtC\(appName.utf16.count)\(appName)\(className.count)\(className)"
-            // return the class!
-            return NSClassFromString(classStringName)
+            let fAppName = appName.replacingOccurrences(of: " ", with: "_")
+            return NSClassFromString("\(fAppName).\(className)")
         }
         return nil
     }
