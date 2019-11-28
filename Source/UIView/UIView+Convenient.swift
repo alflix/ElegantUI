a746aa48687364ff84e53a6606626266cf8c56cc
alflix <jieyuanz24@gmail.com>
2019-11-28 23:56:58 +0800

    add drawDottedLine

--- Source/UIView/UIView+Convenient.swift
+++ Source/UIView/UIView+Convenient.swift
@@ -20,3 +20,31 @@ public extension UIView {
         self.backgroundColor = backgroundColor
     }
 }
+
+extension CGRect {
+    init(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat) {
+        self.init(x: x, y: y, width: w, height: h)
+    }
+}
+
+public extension UIView {
+    // TODO: 可封装 -> GGUI
+    /// 添加虚线边框
+    @discardableResult
+    func drawDottedLine(start point0: CGPoint,
+                        end point1: CGPoint,
+                        strokeColor: UIColor? = nil,
+                        lineWidth: CGFloat? = nil,
+                        lineDashPattern: [NSNumber]? = nil) -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = strokeColor?.cgColor ?? UIColor.lightGray.cgColor
+        shapeLayer.lineWidth = lineWidth ?? 1
+        // 4 虚线高, 3 间隔高.
+        shapeLayer.lineDashPattern = lineDashPattern ?? [4, 1]
+        let path = CGMutablePath()
+        path.addLines(between: [point0, point1])
+        shapeLayer.path = path
+        layer.addSublayer(shapeLayer)
+        return shapeLayer
+    }
+}
