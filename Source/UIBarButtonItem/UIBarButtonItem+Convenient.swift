7cf59d56428502ce30282efdfc5b0dc6681ec1db
alflix <jieyuanz24@gmail.com>
2019-12-12 23:58:16 +0800

    update UIBarButtonItem+Convenient

--- Source/UIBarButtonItem/UIBarButtonItem+Convenient.swift
+++ Source/UIBarButtonItem/UIBarButtonItem+Convenient.swift
@@ -9,22 +9,19 @@
 import UIKit
 
 public extension UIBarButtonItem {
-    convenience init(image: UIImage, target: AnyObject, action: Selector) {
-        self.init(image: image, selectImage: nil, target: target, action: action)
-    }
-
     convenience init(image: UIImage,
                      selectImage: UIImage? = nil,
                      contentEdgeInsets: UIEdgeInsets = .zero,
-                     target: AnyObject,
-                     action: Selector) {
+                     actionBlock: VoidBlock? = nil) {
         self.init()
         let barButton = UIButton()
         barButton.setImage(image, for: .normal)
         if let selectImage = selectImage {
             barButton.setImage(selectImage, for: .selected)
         }
-        barButton.addTarget(target, action: action, for: .touchUpInside)
+        barButton.onTap { (_) in
+            actionBlock?()
+        }
         barButton.contentEdgeInsets = contentEdgeInsets
         barButton.sizeToFit()
         customView = barButton
@@ -34,13 +31,39 @@ public extension UIBarButtonItem {
                      color: UIColor,
                      font: UIFont,
                      contentEdgeInsets: UIEdgeInsets = .zero,
-                     target: AnyObject, action: Selector) {
+                     actionBlock: VoidBlock? = nil) {
         self.init()
         let barButton = UIButton()
         barButton.titleLabel?.font = font
         barButton.setTitleColor(color, for: .normal)
         barButton.setTitle(title, for: .normal)
-        barButton.addTarget(target, action: action, for: .touchUpInside)
+        barButton.onTap { (_) in
+            actionBlock?()
+        }
+        barButton.contentEdgeInsets = contentEdgeInsets
+        barButton.sizeToFit()
+        self.customView = barButton
+    }
+
+    convenience init(title: String,
+                     image: UIImage,
+                     direction: TitleImageDirection = .left,
+                     space: CGFloat,
+                     color: UIColor,
+                     font: UIFont,
+                     contentEdgeInsets: UIEdgeInsets = .zero,
+                     actionBlock: VoidBlock? = nil) {
+        self.init()
+        let barButton = LayoutButton()
+        barButton.titleLabel?.font = font
+        barButton.setTitleColor(color, for: .normal)
+        barButton.setTitle(title, for: .normal)
+        barButton.setImage(image, for: .normal)
+        barButton.imageDirection = direction
+        barButton.imageTitleSpace = space
+        barButton.onTap { (_) in
+            actionBlock?()
+        }
         barButton.contentEdgeInsets = contentEdgeInsets
         barButton.sizeToFit()
         self.customView = barButton
