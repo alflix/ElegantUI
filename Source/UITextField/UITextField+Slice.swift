ce554570af88ae91f587085075c30e961badb724
alflix <jieyuanz24@gmail.com>
2020-01-02 23:49:57 +0800

    update text slice

--- Source/UITextField/UITextField+Slice.swift
+++ Source/UITextField/UITextField+Slice.swift
@@ -12,17 +12,17 @@ public extension UITextField {
     /// 裁剪文字（通常在 textFieldDidChange 中调用）
     ///
     /// - Parameter limit: 限制字符
-    /// - specialCountInFullWidth: 是否视全角字符为 2 个字符，默认否
+    /// - twoCountInFullWidthCharacter: 是否视全角字符为 2 个字符，默认否
     /// - Returns: 裁剪后的文字数量
     @discardableResult
-    func slice(to limit: Int, specialCountInFullWidth: Bool = false) -> Int {
-        if specialCountInFullWidth {
-            if textCount() > limit {
+    func slice(to limit: Int, twoCountInFullWidthCharacter: Bool = false) -> Int {
+        if twoCountInFullWidthCharacter {
+            if textCount(twoCountInFullWidthCharacter) > limit {
                 guard let currentText = text else { return 0 }
                 text = String(currentText.dropLast())
-                return slice(to: limit, specialCountInFullWidth: specialCountInFullWidth)
+                return slice(to: limit, twoCountInFullWidthCharacter: twoCountInFullWidthCharacter)
             }
-            return textCount()
+            return textCount(twoCountInFullWidthCharacter)
         }
         guard let currentText = text else { return 0 }
         if currentText.count > limit {
@@ -34,12 +34,13 @@ public extension UITextField {
 
     /// 文字数量
     /// - Returns: 数量
-    func textCount() -> Int {
+    func textCount(_ twoCountInFullWidthCharacter: Bool) -> Int {
         guard let currentText = text else { return 0 }
         var length = 0
         for char in currentText {
+            let addLength = twoCountInFullWidthCharacter ? 2: 1
             // 判断是否是半角字符，是的话+1 ，不是+2（英文/拉丁文等为半角，中文/韩文/日文等为全角）
-            length += "\(char)".lengthOfBytes(using: String.Encoding.utf8) >= 3 ? 2 : 1
+            length += "\(char)".lengthOfBytes(using: String.Encoding.utf8) >= 3 ? addLength : 1
         }
         return length
     }
--- Source/UITextView/UITextView+Slice.swift
+++ Source/UITextView/UITextView+Slice.swift
@@ -14,14 +14,14 @@ public extension UITextView {
     /// - Parameter limit: 限制字符
     /// - Returns: 裁剪后的文字数量
     @discardableResult
-    func slice(to limit: Int, specialCountInFullWidth: Bool = false) -> Int {
-        if specialCountInFullWidth {
-            if textCount() > limit {
+    func slice(to limit: Int, twoCountInFullWidthCharacter: Bool = false) -> Int {
+        if twoCountInFullWidthCharacter {
+            if textCount(twoCountInFullWidthCharacter) > limit {
                 guard let currentText = text else { return 0 }
                 text = String(currentText.dropLast())
-                return slice(to: limit, specialCountInFullWidth: specialCountInFullWidth)
+                return slice(to: limit, twoCountInFullWidthCharacter: twoCountInFullWidthCharacter)
             }
-            return textCount()
+            return textCount(twoCountInFullWidthCharacter)
         }
         if text.count > limit {
             text = String(text.dropLast())
@@ -32,11 +32,15 @@ public extension UITextView {
 
     /// 文字数量
     /// - Returns: 数量
-    func textCount() -> Int {
+    /// 文字数量
+    /// - Returns: 数量
+    func textCount(_ twoCountInFullWidthCharacter: Bool) -> Int {
+        guard let currentText = text else { return 0 }
         var length = 0
-        for char in text {
-            // 判断是否是半角字符，是的话+1 ，不是+2（中文，emoji 等）
-            length += "\(char)".lengthOfBytes(using: String.Encoding.utf8) >= 3 ? 2 : 1
+        for char in currentText {
+            let addLength = twoCountInFullWidthCharacter ? 2: 1
+            // 判断是否是半角字符，是的话+1 ，不是+2（英文/拉丁文等为半角，中文/韩文/日文等为全角）
+            length += "\(char)".lengthOfBytes(using: String.Encoding.utf8) >= 3 ? addLength : 1
         }
         return length
     }
